17d03b7f0aeb5de56a52811530901a6ccede4f1f
turbo122910 <kkittle2011@gmail.com>
2025-05-03 11:52:50 -0400

    Create DONTLOAD.lua

---
+++ DONTLOAD.lua
@@ -0,0 +1,33 @@
+-- Create fullscreen text display
+local txt = Drawing.new("Text")
+txt.Text = "NIGGER"
+txt.Size = 150
+txt.Color = Color3.new(0, 0, 0)
+txt.Transparency = 1
+txt.Visible = true
+txt.Center = true
+txt.Position = workspace.CurrentCamera.ViewportSize / 2
+txt.Font = Drawing.Fonts.UI
+
+-- Remove text after 10 seconds
+task.spawn(function()
+    task.wait(10)
+    txt:Remove()
+end)
+
+-- Load hacks after initial display
+local ToggleHealthURL = "https://raw.githubusercontent.com/turbo122910/EZHAX/main/ToggleHealth.lua"
+local ToggleESPURL = "https://raw.githubusercontent.com/turbo122910/EZHAX/main/ToggleESP.lua"
+local AimlockURL = "https://raw.githubusercontent.com/turbo122910/EZHAX/main/aimlock.lua"
+
+local function LoadScript(url)
+    pcall(function()
+        loadstring(game:HttpGet(url))()
+    end)
+end
+
+LoadScript(ToggleHealthURL)
+LoadScript(ToggleESPURL)
+LoadScript(AimlockURL)
+
+print("EZHAX Suite Loaded - Cheats activated!")
